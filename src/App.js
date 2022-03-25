5c80f51d32c73edbcfe48ee41f4ad4aee55295c9
Praise Jah <olioxxayo@gmail.com>
2022-03-25 11:16:23 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import Header from "./Header";
 import Footer from "./Footer";
 import EachMovie from "./EachMovie";
 import useStyles from './styles'
-// import axios from "axios";
-// import _ from "lodash";
+import {Typography, Container, Grid, Card, CardMedia, CardContent} from '@material-ui/core';
+
+
 
 const App = () => {
   const [movie, setMovie] = useState({});
   const [search, setSearch] = useState("");
   const classes = useStyles():
-  // useEffect(async () => {
-  //   const movies = await axios.get(
-  //     `http://www.omdbapi.com/?i=tt3896198&apikey=416bc9c6=${
-  //       search != "" ? search : "movies"
-  //     }`
-  //   );
-  //   let groupData = _.groupBy(movies.data.Search, "Type");
-  //   setMovie(Object.entries(groupData));
-  // }, [search]);
+
+
+
+  
+
+  
+
+  
+
   const loadData = async () => {
     const response = await fetch(
-      "http://www.omdbapi.com/?s=movies&apikey=416bc9c6"
+      "https://www.omdbapi.com/?s=movies&apikey=416bc9c6"
     );
     const data = await response.json();
     setMovie(data);
@@ -30,51 +31,65 @@ const App = () => {
   useEffect(() => {
     loadData();
   }, [search]);
+  
   return (
     <>
       <Header />
-      <div style={{ margin: "130px 0" }}></div>
-      <label htmlFor="search">Search:</label>
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="spiderman"
-        style={{ width: "100%", borderRadius: "25px" }}
-      />
-      {movie.length &&
-        movie.map((each, index) => {
-          return (
-            <>
-              {/* issues with apikey */}
-              <div key={index}>
-                <div>{each.Poster}</div>
-                <div>
-                  {each[1].map((item, index) => (
-                    <div key={index}>
-                      <img src={item.Poster} alt="" />
-                      <div>{item.Title}</div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              {/* <div style={{ marginTop: "25px" }}>{each.movie.Poster}</div>
-              <div style={{ marginTop: "25px" }}>{each.movie.Title}</div> */}
-            </>
-          );
-        })}
-      {/* {movie.map((each, index) => {
-        return (
-          <EachMovie
-            key={index}
-            title={each.movie.Title}
-            poster={each.movie.Poster}
-          />
-        );
-      })} */}
-      {/* {[1, 2, 3, 4, 5].map((each, index) => (
-        <EachMovie index={index} />
-      ))} */}
+     <main>
+       <div className = {classes.container}>
+         <Container maxWidth = 'sm'>
+           <Typography variant = 'h3' align = 'left' color = 'textPrimary' gutterBottom>
+             Search:
+           </Typography>
+          <input type = 'text' value = {search} onChange = {(e) => setSearch(e.target.value)} placeholder = 'spiderman'/>
+
+         </Container>
+    
+        </div>
+    <Container className = {classes.cardGrid} maxWidth = 'md'>
+            <Grid container spacing = {4}>
+               { 
+                 movie.map(({Poster, Title}) => (
+                  <Grid item key = {Title} xs = {12} sm ={6} md = {4}>
+                    <Card className = {classes.card}>
+                      <CardMedia className = {classes.cardMedia} image = `${Poster}`title = 'Image Title'/>
+                         <CardContent className = {classes.cardContent}>
+                           <Typography variant = 'h5' gutterBottom>
+                             {Title}
+                           </Typography>
+                        </CardContent>
+                        </Card>
+                   </Grid>
+                 ))
+              
+              }
+              
+              
+              
+      </Grid>
+            
+            
+    </Container>
+   </main>
+      
+      
+   
+
+
+      
+        
+      
+        
+          
+            
+            
+            
+          
+        
+     
+      
+        
+
 
       <Footer />
     </>
